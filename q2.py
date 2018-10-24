93084df614e4c59335664b346d641d6f942a309a
dka316 <dka316@ic.ac.uk>
2018-10-24 19:59:48 +0100

    Naive q2

--- q2.py
+++ q2.py
@@ -1,10 +1,12 @@
-# ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
-
 import numpy as np
+import itertools as it
 
-#compute all combinations for two portfolios
 def question02(cashFlowIn, cashFlowOut):
-  # modify and then return the variable below
-  answer = -1
-  return answer
+  ins = map(sum, powerset(cashFlowIn))
+  outs = map(sum, powerset(cashFlowOut))
+  return min([abs(_in - _out) for _in, _out in it.product(ins, outs)][1:])
 
+def powerset(set):
+  ls = list(set)
+  ln = len(ls)
+  return it.chain.from_iterable(it.combinations(ls, r) for r in range(ln + 1))
