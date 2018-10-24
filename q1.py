d4dd51510b022a5fade077b2d79f43cbdf12b776
dka316 <dka316@ic.ac.uk>
2018-10-24 18:34:43 +0100

    Naive q1

--- q1.py
+++ q1.py
@@ -1,9 +1,10 @@
-# ONLY EDIT FUNCTIONS MARKED CLEARLY FOR EDITING
-
 import numpy as np
+import itertools as it
 
-# modify this function, and create other functions below as you wish
 def question01(portfolios):
-  # modify and then return the variable below
-  answer = 0
-  return answer
+  max_value = 0
+  for (portfolio_1, portfolio_2) in it.combinations(portfolios, r = 2):
+    new_value = portfolio_1 ^ portfolio_2
+    if new_value > max_value:
+      max_value = new_value
+  return max_value
